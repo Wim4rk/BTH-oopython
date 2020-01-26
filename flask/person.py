8bc54be820daec2fb86ceb2bab12742f1097f7b4
Wim4rk <olov.wimark@gmail.com>
2020-01-26 16:17:00 +0100

    Object person is working

--- flask/person.py
+++ flask/person.py
@@ -8,16 +8,32 @@ for serving up to Flask-driven webpage.
 
 import json
 
-class Person(){
+class Person():
     """
     Contains data describing a person
     """
+
+    firstName = ''
+    lastName = ''
+    birthDate = ''
+    image = ''
 
     def __init__(self):
         self.get_person()
 
     def get_person(self):
-        json_data = json.load(open("olov.json", encoding="utf-8"))
+        # json_data = json.load(open("olov.json", encoding="utf-8"))
+        # firstName = json_data["firstN"]
+        # lastName = json_data["lastN"]
+        # birthDate = json_data["birthD"]
+        # image = json_data["img"]
 
+        self.firstName = 'Olov'
+        self.lastName = 'Wimark'
 
-}
+    def print_name(self):
+        print(self.firstName, " ", self.lastName)
+
+if __name__ == "__main__":
+    pn = Person()
+    pn.print_name()
